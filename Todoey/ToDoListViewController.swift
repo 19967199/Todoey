e764910b9d3aac67cc232f672b3368af07c1f6d3
José Alberto Álvarez Morán <josealbertoalvarezmoran@macbook-air-de-jose.local>
2018-09-17 13:48:19 +0200

    Añadir nuevo elemento

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Hola", "que", "tal"]
+    var itemArray = ["Hola", "que", "tal"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,29 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            // Lo que pasa cuando se presiona el botón en la alerta
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
     
 }
 
