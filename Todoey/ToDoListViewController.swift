80448a96a4a34fd139e38da7f9d817bd126cd759
José Alberto Álvarez Morán <josealbertoalvarezmoran@macbook-air-de-jose.local>
2018-09-17 14:12:45 +0200

    Data persisted using Defaults

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,8 +12,15 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Hola", "que", "tal"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -48,6 +55,9 @@ class ToDoListViewController: UITableViewController {
             // Lo que pasa cuando se presiona el botón en la alerta
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
         }
         
