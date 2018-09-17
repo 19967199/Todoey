42274d2da7883963acfa25ae8c99feb7a9586828
José Alberto Álvarez Morán <josealbertoalvarezmoran@macbook-air-de-jose.local>
2018-09-17 18:46:52 +0200

    Convert to MVC

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by José Alberto Álvarez Morán on 17/9/18.
+//  Copyright © 2018 José Alberto Álvarez Morán. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
